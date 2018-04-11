201a1ade2c42b9d5addc0099019fbf625815b954
叶兵锋 <yebingfeng@bijianhuzhu.com>
2018-04-11 15:02:08 +0800

    Initial Commit

---
+++ runtime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  runtime
+//
+//  Created by 叶兵锋 on 2018/4/11.
+//  Copyright © 2018年 yebingfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ runtime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  runtime
+//
+//  Created by 叶兵锋 on 2018/4/11.
+//  Copyright © 2018年 yebingfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
